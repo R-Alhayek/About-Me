fb2a8b6f2cb5cb39f7c952a08bc7bfd7c7f9c448
‘R-Alhayek’ <rafeefalhayek94@outlook.com>
2021-04-26 16:13:34 +0300

    keeping work up to date

--- js/app.js
+++ js/app.js
@@ -19,6 +19,46 @@ switch (study) {
     case 'n':
         console.log('wrong');
         alert('NAH, NOT REALLY!');
-        break;
+        
 
+}
+let age = prompt('Do you think I am above 26?');
+age = age.toLowerCase();
+switch (age) {
+    case 'yes':
+        console.log('correct!');
+        alert('CORRECT!');
+        break;
+    case 'y':
+        console.log('correct!');
+        alert('CORRECT!');
+        break;
+    case 'no':
+        console.log('wrong');
+        alert('NAH, NOT REALLY!');
+        break;
+    case 'n':
+        console.log('wrong');
+        alert('NAH, NOT REALLY!');
+        
+
+}
+let movies = prompt('Do you think I like horror movies?');
+movies = movies.toLowerCase();
+switch (movies) {
+    case 'yes':
+        console.log('correct!');
+        alert('CORRECT!');
+        break;
+    case 'y':
+        console.log('correct!');
+        alert('CORRECT!');
+        break;
+    case 'no':
+        console.log('wrong');
+        alert('NAH, NOT REALLY!');
+        break;
+    case 'n':
+        console.log('wrong');
+        alert('NAH, NOT REALLY!');
 }
