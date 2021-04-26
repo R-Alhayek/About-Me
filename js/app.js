84ca46964a244ccffc4dc77a275025a0fe46f246
‘R-Alhayek’ <rafeefalhayek94@outlook.com>
2021-04-26 16:30:54 +0300

    keeping work up to date

--- js/app.js
+++ js/app.js
@@ -5,20 +5,20 @@ let study = prompt('Do you think I studied programming at univirsity (yes,no)?')
 study = study.toLowerCase();
 switch (study) {
     case 'yes':
-        console.log('correct!');
-        alert('CORRECT!');
+        //console.log('wrong!');//
+        alert('NAH, NOT REALLY!!');
         break;
     case 'y':
-        console.log('correct!');
-        alert('CORRECT!');
+        //console.log('wrong!');//
+        alert('NAH, NOT REALLY!!');
         break;
     case 'no':
-        console.log('wrong');
-        alert('NAH, NOT REALLY!');
+        //console.log('true');//
+        alert('CORRECT!');
         break;
     case 'n':
-        console.log('wrong');
-        alert('NAH, NOT REALLY!');
+        //console.log('true');//
+        alert('CORRECT!');
         
 
 }
@@ -26,19 +26,19 @@ let age = prompt('Do you think I am above 26(yes,no)?');
 age = age.toLowerCase();
 switch (age) {
     case 'yes':
-        console.log('correct!');
+        //console.log('correct!');//
         alert('CORRECT!');
         break;
     case 'y':
-        console.log('correct!');
+        //console.log('correct!');//
         alert('CORRECT!');
         break;
     case 'no':
-        console.log('wrong');
+        //console.log('wrong');//
         alert('NAH, NOT REALLY!');
         break;
     case 'n':
-        console.log('wrong');
+        //console.log('wrong');//
         alert('NAH, NOT REALLY!');
         
 
@@ -47,56 +47,56 @@ let movies = prompt('Do you think I like horror movies(yes,no)?');
 movies = movies.toLowerCase();
 switch (movies) {
     case 'yes':
-        console.log('correct!');
-        alert('CORRECT!');
+        //console.log('wrong');//
+        alert('NAH, NOT REALLY!');
         break;
     case 'y':
-        console.log('correct!');
-        alert('CORRECT!');
+        //console.log('wrong');//
+        alert('NAH, NOT REALLY!!');
         break;
     case 'no':
-        console.log('wrong');
-        alert('NAH, NOT REALLY!');
+        //console.log('true');//
+        alert('CORRECT!');
         break;
     case 'n':
-        console.log('wrong');
-        alert('NAH, NOT REALLY!');
+        //console.log('true');//
+        alert('CORRECT!');
 }
 let cook = prompt('Do you think I can make good desserts(yes,no)?');
 cook = cook.toLowerCase();
 switch (cook) {
     case 'yes':
-        console.log('correct!');
+        //console.log('correct!');//
         alert('CORRECT!');
         break;
     case 'y':
-        console.log('correct!');
+        //console.log('correct!');//
         alert('CORRECT!');
         break;
     case 'no':
-        console.log('wrong');
+        //console.log('wrong');//
         alert('NAH, NOT REALLY!');
         break;
     case 'n':
-        console.log('wrong');
+        //console.log('wrong');//
         alert('NAH, NOT REALLY!');
 }
 let sing = prompt('Do you think I can sing well(yes,no)?');
 sing = sing.toLowerCase();
 switch (sing) {
     case 'yes':
-        console.log('correct!');
-        alert('CORRECT!');
+        //console.log('wrong');//
+        alert('NAH, NOT REALLY!');
         break;
     case 'y':
-        console.log('correct!');
-        alert('CORRECT!');
+        //console.log('wrong');//
+        alert('NAH, NOT REALLY!');
         break;
     case 'no':
-        console.log('wrong');
-        alert('NAH, NOT REALLY!');
+        //console.log('true');//
+        alert('CORRECT!');
         break;
     case 'n':
-        console.log('wrong');
-        alert('NAH, NOT REALLY!');
+        //console.log('true');//
+        alert('CORRECT!');
 }
